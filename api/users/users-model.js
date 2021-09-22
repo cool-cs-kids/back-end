45ce43b2f96509c5b963e8d06a35a0ac12474079
tryingtokeepup <kl.hawaii@gmail.com>
2021-09-22 12:32:36 -0700

    postgres integration

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -16,7 +16,7 @@ function findById(user_id) {
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user, 'id')
+    const [id] = await db('users').insert(user, 'id').returning('id');
     return findById(id)
 }
 
