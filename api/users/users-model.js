1ce44cf75c1a0790d4097276740c84848319907a
tryingtokeepup <kl.hawaii@gmail.com>
2021-09-22 12:36:29 -0700

    id is not called id, its called user_id

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -16,8 +16,8 @@ function findById(user_id) {
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user, 'id').returning('id');
-    return findById(id)
+    const [user_id] = await db('users').insert(user, 'user_id').returning('user_id');
+    return findById(user_id)
 }
 
 
